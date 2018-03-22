bc111f6b17192d2f11191e7148b5ed9c0cb9dbaf
doerme <398921432@qq.com>
2018-03-22 22:01:05 +0800

    还差底部素材

--- src/js/components/index/userinfo.vue
+++ src/js/components/index/userinfo.vue
@@ -10,6 +10,39 @@
           <p>我是：<span>陈小爱</span>, 我在这里等您</p>
           <p>工号：<span>138907890</span> 手机号：<span>18927268989</span></p>
       </div>
+      <div class="userinfowrap-table">
+          <img class="userinfowrap-table-title" src="./images/table-title.png" />
+          <table class="userinfowrap-table-main">
+              <tr>
+                  <td>88%</td>
+                  <td>88%</td>
+                  <td>88%</td>
+                  <td>88%</td>
+                  <td>88%</td>
+                  <td>88%</td>
+                  <td>88%</td>
+              </tr>
+              <tr>
+                  <td>学习能力</td>
+                  <td>自信心</td>
+                  <td>执行力</td>
+                  <td>创新</td>
+                  <td>意志力</td>
+                  <td>沟通能力</td>
+                  <td>技能</td>
+              </tr>
+          </table>
+          <div class="userinfowrap-table-value">1号主管值 92%</div>
+          <div class="userinfowrap-table-subvalue">
+              <p>陈小爱，你的自信心爆表，2018创业</p>
+              <p>会找的就是你，加入我们吧！</p>
+          </div>
+          <div class="userinfowrap-table-btwrap">
+              <div class="bt-like">300</div>
+              <div class="bt-left">明星排行榜</div>
+              <div class="bt-right">我也要玩</div>
+          </div>
+      </div>
   </div>
 </template>
 
@@ -66,6 +99,92 @@ $font_size : 108;
 .userinfowrap{
     position: relative;
     height: rem(1980);
+    &-table{
+        position: absolute;
+        top: rem(1120);
+        left: 0;
+        width: 100%;
+        &-value{
+            font-size: rem(48);
+            color: #efa40c;
+            text-align: center;
+            padding-top: rem(40);
+        }
+        &-subvalue{
+            font-size: rem(42);
+            color: #FFF;
+            text-align: center;
+        }
+        &-btwrap{
+            position: relative;
+            width: 100%;
+            .bt-like{
+                position: absolute;
+                width: rem(235);
+                height: rem(235);
+                left: 50%;
+                transform: translateX(-50%);
+                background: url('./images/zan-bt.png') no-repeat;
+                background-size: 100% auto; 
+                font-size: rem(36);
+                color: #3758f1;
+                text-align: center;
+                line-height: rem(330);
+                &:active{
+                    opacity: 0.8;
+                }
+            }
+            .bt-left{
+                position: absolute;
+                background: #4295f3;
+                width: rem(330);
+                height: rem(90);
+                font-size: rem(48);
+                color: #FFF;
+                text-align: center;
+                line-height: rem(90);
+                top: rem(280);
+                left: rem(170);
+                border-radius: rem(80);
+                &:active{
+                    opacity: 0.8;
+                }
+            }
+            .bt-right{
+                position: absolute;
+                background: #4295f3;
+                width: rem(330);
+                height: rem(90);
+                font-size: rem(48);
+                color: #FFF;
+                text-align: center;
+                line-height: rem(90);
+                top: rem(280);
+                right: rem(170);
+                border-radius: rem(80);
+                &:active{
+                    opacity: 0.8;
+                }
+            }
+        }
+        &-title{
+            width: 100%;
+        }
+        &-main{
+            width: rem(1050);
+            font-size: rem(32);
+            font-family: "Adobe Heiti Std";
+            color: rgb(255, 255, 255);
+            font-variant: small-caps;
+            text-align: center;
+            margin-left: auto;
+            margin-right: auto;
+            text-shadow: 0 rem(3) #395d93, rem(3) 0#395d93, rem(-3) 0#395d93, 0 rem(-3)#395d93;
+            td{
+                width: 14.2%;
+            }
+        }
+    }
     &-note{
         position: absolute;
         width: 100%;
